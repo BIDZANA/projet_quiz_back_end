e54cd7bf888109c28207e2da4c54bdf9a5e7acb5
Marvin BIDZANA <marvinperan.bidzanabidzana@epfedu.fr>
2023-10-16 11:28:37 +0200

    Initialisation du projet

---
+++ quiz-backend/src/test/java/epf/edu/quizbackend/QuizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package epf.edu.quizbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
